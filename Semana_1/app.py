7b46554c24ae4cea2aee950bb6afb7e4754544a9
chartie87742 <chartie87gg@gmail.com>
2026-09-21 20:19:42 -0600

    Error Corregido

--- Semana_1/app.py
+++ Semana_1/app.py
@@ -24,4 +24,4 @@ if st.button("Predecir"):
     resultado = modelo_lr.predict([[gasto]])
 
 # streamlit muestra un mensaje de exito en verde bonito usando success
-st.success(f"Las ventas proyectadas para una inversion de ${gasto} son:${resultado[0]}")
+    st.success(f"Las ventas proyectadas para una inversion de ${gasto} son:${resultado[0]}")
